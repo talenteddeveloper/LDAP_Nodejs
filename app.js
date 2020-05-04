0f513681c239ffc33f4fd3996736b970d0b399ca
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-05-04 20:16:51 +0530

    Create connection and connect with ldapjs

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
+var ldap = require('ldapjs');
 
 app.listen(3000, function () {
     console.log("server started")
 })
+
+function authenticateDN(username, password){
+    var client = ldap.createClient({
+        url: 'ldap://127.0.0.1:10389'
+    });
+
+    client.bind(username, password, function(err) {
+        if(err)
+        {
+            console.log("Error in new connetion "+err)
+        }else {
+            console.log("Success");
+
+        }
+    });
+}
+
+authenticateDN("uid=admin,ou=system","secret")
