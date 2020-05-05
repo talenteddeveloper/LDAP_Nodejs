a4832d93e8e124a56be3f04dbe48046e1616ff76
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-05-05 19:37:53 +0530

    get all user with ldapjs

--- app.js
+++ app.js
@@ -10,14 +10,38 @@ function authenticateDN(username, password){
     var client = ldap.createClient({
         url: 'ldap://127.0.0.1:10389'
     });
-
+    var opts = {
+        filter: '(objectClass=*)',
+        scope: 'sub',
+        attributes: ['sn']
+    };
     client.bind(username, password, function(err) {
         if(err)
         {
             console.log("Error in new connetion "+err)
         }else {
             console.log("Success");
+            client.search('ou=users,ou=system', opts, function(err, res) {
+                if(err)
+                {
+                    console.log("Error in search "+err)
+                }else {
+                    res.on('searchEntry', function(entry) {
+                        console.log('entry: ' + JSON.stringify(entry.object));
+                    });
+                    res.on('searchReference', function(referral) {
+                        console.log('referral: ' + referral.uris.join());
+                    });
+                    res.on('error', function(err) {
+                        console.error('error: ' + err.message);
+                    });
+                    res.on('end', function(result) {
+                        console.log('status: ' + result.status);
+                    });
+                }
 
+
+            });
         }
     });
 }
