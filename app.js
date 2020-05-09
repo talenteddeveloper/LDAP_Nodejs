29e0812e7b0ba5bbf1011ac8ec9c5c66859bab96
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-05-10 01:03:37 +0530

    format code & add user

--- app.js
+++ app.js
@@ -5,11 +5,26 @@ var ldap = require('ldapjs');
 app.listen(3000, function () {
     console.log("server started")
 })
-
+var client = ldap.createClient({
+    url: 'ldap://127.0.0.1:10389'
+});
 function authenticateDN(username, password){
-    var client = ldap.createClient({
-        url: 'ldap://127.0.0.1:10389'
+
+
+    client.bind(username, password, function(err) {
+        if(err)
+        {
+            console.log("Error in new connetion "+err)
+        }else {
+            console.log("Success");
+            //searchUser();
+            addUser();
+        }
     });
+}
+
+function searchUser()
+{
     var opts = {
         //  filter: '(objectClass=*)',  //simple search
         //  filter: '(&(uid=2)(sn=John))',// and search
@@ -17,35 +32,48 @@ function authenticateDN(username, password){
         scope: 'sub',
         attributes: ['sn']
     };
-    client.bind(username, password, function(err) {
+    client.search('ou=users,ou=system', opts, function(err, res) {
         if(err)
         {
-            console.log("Error in new connetion "+err)
+            console.log("Error in search "+err)
         }else {
-            console.log("Success");
-            client.search('ou=users,ou=system', opts, function(err, res) {
-                if(err)
-                {
-                    console.log("Error in search "+err)
-                }else {
-                    res.on('searchEntry', function(entry) {
-                        console.log('entry: ' + JSON.stringify(entry.object));
-                    });
-                    res.on('searchReference', function(referral) {
-                        console.log('referral: ' + referral.uris.join());
-                    });
-                    res.on('error', function(err) {
-                        console.error('error: ' + err.message);
-                    });
-                    res.on('end', function(result) {
-                        console.log('status: ' + result.status);
-                    });
-                }
-
-
+            res.on('searchEntry', function(entry) {
+                console.log('entry: ' + JSON.stringify(entry.object));
             });
+            res.on('searchReference', function(referral) {
+                console.log('referral: ' + referral.uris.join());
+            });
+            res.on('error', function(err) {
+                console.error('error: ' + err.message);
+            });
+            res.on('end', function(result) {
+                console.log('status: ' + result.status);
+            });
+        }
+
+
+    });
+
+}
+
+function addUser()
+{
+    var entry = {
+
+        sn: 'bar',
+        email: ['foo@bar.com', 'foo1@bar.com'],
+        objectclass: 'inetOrgPerson'
+    };
+    client.add('cn=foo12,ou=users,ou=system', entry, function(err) {
+        if(err)
+        {
+            console.log("err in new user "+err);
+        }else
+        {
+            console.log("added user")
         }
     });
 }
+
 
 authenticateDN("uid=admin,ou=system","secret")
